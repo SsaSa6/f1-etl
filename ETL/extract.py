4a2818c6fc31f9f122b0678f9687462f2089381d
SsaSa6 <zxcvandandand1@Gmail.com>
2026-06-09 21:15:16 +0900

    extract파일 리팩토링함!

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,16 +1,23 @@
 import requests
 import json
 import pandas as pd
+import os
 
-url = "https://api.openf1.org/v1/drivers?session_key=latest" #api를 호출
-response = requests.get(url) #api 결과를 딕셔너리 형태로 저장
+endpoint = input("가져올 데이터의 이름 : ") #가져올 데이터명
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_dir, f"f1_all_{endpoint}.json")
 
-if response.status_code != 200: #api호출이 성공적이면 else로 아니면 호출 이유를 설명
-    print(f"API 호출 실패: {response.status_code}")
+def get_f1_data(endpoint):
+    url = f"https://api.openf1.org/v1/{endpoint}" 
+    response = requests.get(url)               
+    return response  
+
+f1_data = get_f1_data(endpoint)
+
+if f1_data.status_code != 200:
+    print(f"API 호출 실패: {f1_data.status_code}")
 else:
-    f1_data = response.json() #response를 json 형태로 변환하여 f1_data에 저장
+    f1_data_end = f1_data.json()
 
-    with open("f1_data.json", "w", encoding="utf-8") as file: 
-        #f1_data를 json 파일로 저장하고 "w"는 쓰기 모드, encoding은 utf-8로 설정
-        json.dump(f1_data, file, indent=4, ensure_ascii=False)
-        #json.dump()는 f1_data를 file에 json 형식으로 저장, indent는 들여쓰기, ensure_ascii는 비영어 문자를 그대로 저장하도록 설정
+    with open(file_path,"w",encoding="utf-8") as file:
+        json.dump(f1_data_end,file,indent=4,ensure_ascii=False)
