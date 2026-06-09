43341e3e0426662381271de957d97fc8f9fddf31
SsaSa6 <zxcvandandand1@Gmail.com>
2026-06-09 23:10:45 +0900

    2025년형으로 데이터 뽑게 extract.py 수정

--- ETL/extract.py
+++ ETL/extract.py
@@ -6,7 +6,7 @@ import os
 endpoint = input("가져올 데이터의 이름 : ") #가져올 데이터명
 parameters = "?year=2025"
 base_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(base_dir, f"f1_all_{endpoint}.json")
+file_path = os.path.join(base_dir, f"f1_2025_{endpoint}.json")
 
 def get_f1_data(endpoint,parameters):
     url = f"https://api.openf1.org/v1/{endpoint}{parameters}" 
